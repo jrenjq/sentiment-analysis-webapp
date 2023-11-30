8360a91a0c98658dd2aa24cf15b89d240a3d1b24
jrenjq <jrenjq@gmail.com>
2023-11-30 18:27:49 +0800

    added Flask rate limiter

--- flask/services/web/project/__init__.py
+++ flask/services/web/project/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
 
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+@app.route("/slow")
+@limiter.limit("1 per day")
+def slow():
+    return ":("
