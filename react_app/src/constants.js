aea838c05c6d9d270b1ddfffcfc5d4471399f938
jrenjq <jrenjq@gmail.com>
2023-12-01 16:02:53 +0800

    new build

--- react_app/src/constants.js
+++ react_app/src/constants.js
@@ -1,2 +1,3 @@
-export const FLASK_URI = "http://localhost" // deployment via docker compose.
+export const FLASK_URI = "http://157.230.33.109" // deployment via docker compose in production.
+// export const FLASK_URI = "http://localhost" // deployment via docker compose in development.
 // export const FLASK_URI = "http://localhost:5000" // development ie. not using docker compose.
